8cce367056e3680ec88b0da810468f88cfb48ca8
Quentin BRATEAU <quentin.brateau@ensta-bretagne.fr>
2022-02-16 13:57:08 +0100

    Minimal Example

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <xtensor/xtensor.hpp>
+
+int main(void) {
+    std::cout << "GCC Compiled Program" << std::endl;
+    return 0;
+}
---
+++ main.cu
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <xtensor/xtensor.hpp>
+
+int main(void) {
+    std::cout << "NVCC Compiled Program" << std::endl;
+    return 0;
+}
